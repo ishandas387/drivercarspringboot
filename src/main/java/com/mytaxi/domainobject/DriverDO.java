e651a3080fc3852adef6427ef7d9b9b79aa0b9c6
ishandas387 <ishan.das387@gmail.com>
2019-06-08 23:59:46 +0530

    generated value

--- src/main/java/com/mytaxi/domainobject/DriverDO.java
+++ src/main/java/com/mytaxi/domainobject/DriverDO.java
@@ -9,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
@@ -24,7 +25,7 @@ public class DriverDO
 {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
